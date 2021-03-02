d2c9efecd563705f2ccfaa86a024b14c6559dca5
Tarik-JS <68224682+Tarik-JS@users.noreply.github.com>
2021-03-02 19:38:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
 const random = require("random")
+const { MessageAttachment } = require("discord.js");
 const canvacord = require("canvacord");
 let prefix = "!";
 
